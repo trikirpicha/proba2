bccb55a02b0d79d4282c347df59f6ccf8c238d93
trikirpicha <trikirpicha1992@gmail.com>
2022-06-14 22:49:07 +0300

    123

--- src/lesson21/Task7.java
+++ src/lesson21/Task7.java
@@ -4,5 +4,6 @@ public class Task7 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("alex");
+        System.out.println("alex");
     }
 }
